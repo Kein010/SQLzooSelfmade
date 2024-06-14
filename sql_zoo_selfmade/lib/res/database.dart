427c459479b38f28a9075b3ee3093faa9b537931
Kein010 <k.g.koschinski@gmail.com>
2024-06-14 08:30:33 +0200

    Fix

--- sql_zoo_selfmade/lib/res/database.dart
+++ sql_zoo_selfmade/lib/res/database.dart
@@ -1,30 +1,31 @@
 import 'package:mysql1/mysql1.dart';
 
-//MySqlConnection connection;
+// MySqlConnection connection;
 
-try {
-  final connection = await MySqlConnection.connect(
-    ConnectionSettings(
-      host: 'localhost',//your_localhost
-      port: 8080,//your_port
-      user: 'root',//your_user
-      password: 'superSecret',//your_password
-      db: 'testdb',//your_database
-    ),
-  );
-} catch (e) {
-  print(e);
-}
+// try {
+//   final connection = await MySqlConnection.connect(
+//     ConnectionSettings(
+//       host: 'localhost',//your_localhost
+//       port: 8080,//your_port
+//       user: 'root',//your_user
+//       password: 'superSecret',//your_password
+//       db: 'testdb',//your_database
+//     ),
+//   );
+// } catch (e) {
+//   print(e);
+// }
 
-String query = 'SELECT * FROM your_table WHERE your_column LIKE %?%';
-List<List<dynamic>> results;
+// String query = 'SELECT * FROM your_table WHERE your_column LIKE %?%';
+// List<List<dynamic>> results;
 
-try {
-  results = await connection.query(query, ['your_search_term']);
-} catch (e) {
-  print(e);
-}
+// try {
+//   results = await connection.query(query, ['your_search_term']);
+// } catch (e) {
+//   print(e);
+// }
 
-for (var row in results) {
-  // Access the columns using row[column_index]
-  // For example: row[0] for the first column
+// for (var row in results) {
+//   // Access the columns using row[column_index]
+//   // For example: row[0] for the first column
+//   }
